e96d831459905165a54c80b0afa9b4f5118d3d75
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-03-28 15:09:06 -0500

    Added comments section

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,22 @@
 public class Program {
+
+
+    // Comments can be written using "//" to precede the comments.
+
+    /* Alternative way to do comments */
+
+    /*
+       Here is a
+       multi-line
+       comment
+     */
+
+    /**
+     * Writing comments like this allows you to utilize JavaDocs
+     * JavaDocs can be used to create HTML based documentation
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World");  // You can even put comments after a command.
     }
 }
