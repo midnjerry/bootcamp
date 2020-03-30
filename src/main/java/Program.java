79627290460ea2d15b2d2020b1f9f9d3f81f9be3
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-03-30 14:17:37 -0500

    Added the class notes for 3/30/2020

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -15,6 +15,41 @@ public class Program {
      * @param args
      */
     public static void main(String[] args) {
+        //Primitive variables
+        boolean isFull = false;
+        byte number = (byte) 257; // [0 - 255]  256 = 1 0000 0001b
+        char ch = 'a';                       // Usually Strings are used.
+        short shortInteger = Short.MAX_VALUE;
+        int maxInteger = Integer.MAX_VALUE;  //Popular
+        long maxLong = Long.MAX_VALUE;        //Popular Database IDs, anything big
+        float maxFloat = Float.MAX_VALUE;
+        double maxDouble = Double.MAX_VALUE;  // Popular
+
+        for (char c = ch; c <= 'z'; c++) {
+            System.out.print(c);
+        }
+
+        System.out.println("\nShort: " + shortInteger);
+        System.out.println("\nMax Integer: " + maxInteger);
+        System.out.println(maxLong);
+        System.out.println(maxFloat);
+        System.out.println(maxDouble);  // Comment
+
+        //Reference variables
+        Boolean b = Boolean.parseBoolean("TrUe");
+        Byte minByte = Byte.MIN_VALUE;
+        Character character = Character.forDigit(2, 2);
+        Character dVariable = 'd';
+        Character bigDVariable = Character.toUpperCase(dVariable);
+        Integer max = Integer.max(45, maxInteger);
+        Double stringValue = Double.parseDouble("234.234");
+
+        System.out.println("Boolean b = " + b);
+        System.out.println(minByte);
+        System.out.println("character: " + character);
+        System.out.println(bigDVariable + ":" +  dVariable);
+        System.out.println(max);
+        System.out.println(stringValue + 50);
         System.out.println("Hello World");  // You can even put comments after a command.
     }
 }
